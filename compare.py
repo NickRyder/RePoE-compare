c7e523a8f1741bd5417f0fb1353106b15123e908
Nick Ryder <nick.ryder@openai.com>
2019-09-06 11:59:21 -0700

    added support for other jsons

--- compare.py
+++ compare.py
@@ -2,27 +2,11 @@ import json
 import difflib
 import markdown
 
-def compare(new_file, old_file):
-    new_file_json = json.load(open(new_file, "r"))
-    old_file_json = json.load(open(old_file, "r"))
 
-    print_string = "[TOC]\n"
-
-    new_keys = set(new_file_json.keys())
-    old_keys = set(old_file_json.keys())
-
-    deleted_keys = old_keys - new_keys
-    added_keys = new_keys - old_keys
-    same_keys = set.intersection(new_keys,old_keys)
-
-    if len(deleted_keys) > 0:
-        print_string += "# DELETED MODS\n"
-        for deleted_key in deleted_keys:
-            print_string += f"#### {deleted_key}\n"
-            print_string += f"```\n{json.dumps(old_file_json[deleted_key], indent=4, sort_keys=True)}\n```\n"
-
+def generate_added_keys_for_mods(added_keys, new_file_json):
+    print_string = ""
     if len(added_keys) > 0:
-        print_string += "# ADDED MODS\n"
+        print_string += "# ADDED\n"
 
 
         added_dict = {}
@@ -43,27 +27,68 @@ def compare(new_file, old_file):
                 for added_key in added_dict[added_domain][added_generation_type]:
                     print_string += f"#### {added_key}\n"
                     print_string += f"```\n {json.dumps(new_file_json[added_key], indent=4, sort_keys=True)}\n```\n"
+    return print_string
 
-    print_string += f"# CHANGED MODS\n"
+def compare(new_file, old_file, mods=True):
+    new_file_json = json.load(open(new_file, "r"))
+    old_file_json = json.load(open(old_file, "r"))
+
+    print_string = ""
+
+    new_keys = set(new_file_json.keys())
+    old_keys = set(old_file_json.keys())
+
+    deleted_keys = old_keys - new_keys
+    added_keys = new_keys - old_keys
+    same_keys = set.intersection(new_keys,old_keys)
+
+    if len(deleted_keys) > 0:
+        print_string += "# DELETED\n"
+        for deleted_key in deleted_keys:
+            print_string += f"#### {deleted_key}\n"
+            print_string += f"```\n{json.dumps(old_file_json[deleted_key], indent=4, sort_keys=True)}\n```\n"
+
+    if mods:
+        print_string += generate_added_keys_for_mods(added_keys,new_file_json)
+    else:
+        if len(added_keys) > 0:
+            print_string += "# ADDED\n"
+            for added_key in added_keys:
+                print_string += f"#### {added_key}\n"
+                print_string += f"```\n{json.dumps(new_file_json[added_key], indent=4, sort_keys=True)}\n```\n"
+
+
+    print_string += f"# CHANGED\n"
     for same_key in same_keys:
         if str(new_file_json[same_key]) != str(old_file_json[same_key]):
             new_item = new_file_json[same_key]
             old_item = old_file_json[same_key]
 
             print_string += f"#### {same_key}\n"
-            for key in new_item:
-                if str(new_item[key]) != str(old_item[key]):
+            all_keys = set(new_item.keys()).union(set(old_item.keys()))
+            for key in all_keys:
+                new_key = new_item.get(key,"")
+                old_key = old_item.get(key,"")
+                if str(new_key) != str(old_key):
                     print_string += f"##### {key}\n"
                     print_string += f"```\n-new-\n"
-                    print_string += f"{new_item[key]} \n"
+                    print_string += f"{new_key} \n"
                     print_string += f"-old-\n"
-                    print_string += f"{old_item[key]} \n```\n"
+                    print_string += f"{old_key} \n```\n"
 
 
     return print_string
 
 
+import md_toc
 
 if __name__ == "__main__":
-    md_compare = compare("3.7.0/mods.json", "3.6.0/mods.json")
-    print(md_compare)
+    compare_file = "mods"
+
+
+    md_compare = compare(f"3.7.0/{compare_file}.json", f"3.6.0/{compare_file}.json",mods=False)
+
+    output_file = f"3.6.0_3.7.0_{compare_file}.md"
+    with open(output_file, "w") as f:
+        f.write(md_compare)
+    print(md_toc.build_toc(output_file))
